594bb78c98e23f8feb74fb484bc445256a9be202
Manuel V <manueldeveloper17@gmail.com>
2022-01-20 17:16:43 -0500

    Se realizó la configuración de algunos archivos

--- storenextjs/next.config.js
+++ storenextjs/next.config.js
@@ -1,3 +1,19 @@
+// En este archivo podemos hacer muchas configuraciones de Next.js
+
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    env: {
+        customKey: 'customValue',
+    },
+    basePath: '/dist',
+    compress: true,
+    async redirects() {
+        return [{
+            // Fuente (1. A partir de esta ruta)
+            source: '/hola',
+            // Destino (2. Se redireccionara a esta ruta)
+            destination: '/hello',
+            permanent: true
+        }]
+    }
 }
